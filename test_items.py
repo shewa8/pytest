b4fc39be3d8eadc0c852f9504b9b7b8d7e76676f
shewa8 <shevchenko.vlad8@gmail.com>
2020-01-04 16:40:14 +0200

    Test file

--- test_items.py
+++ test_items.py
@@ -8,7 +8,7 @@ def test_find__card_button(browser):
     # Для удобства, просто в строке ниже удалите "#"
     #time.sleep(30)
 
-    basket_btn = len(browser.find_elements_by_class_name("btn-add-to-basket"))
-    assert basket_btn > 0, 'Element isn\'t found'
+    card_btn = len(browser.find_elements_by_class_name("btn-add-to-basket"))
+    assert card_btn > 0, 'Element isn\'t found'
 
 
