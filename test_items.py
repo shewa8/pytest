a31b8b915019d499b49642cda6381872e67bd4de
shewa8 <shevchenko.vlad8@gmail.com>
2020-01-04 16:38:09 +0200

    Test file

--- test_items.py
+++ test_items.py
@@ -2,7 +2,7 @@ import time
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_browser_language(browser):
+def test_find__card_button(browser):
     browser.get(link)
 
     # Для удобства, просто в строке ниже удалите "#"
