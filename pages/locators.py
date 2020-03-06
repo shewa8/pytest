0c54aaf6d20674df33cf58572ee760d0e2a5c29a
shewa8 <shevchenko.vlad8@gmail.com>
2020-03-06 15:45:09 +0200

    Added methods in base_page, now is possible go from any web-page to login page

--- pages/locators.py
+++ pages/locators.py
@@ -1,9 +1,22 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+
+
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
 
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FROM = (By.CSS_SELECTOR, "#register_form")
+
+
+class ProductPageLocatots():
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, "h1:first-child")
+    PRODUCT_IN_BASKET = (By.CSS_SELECTOR, ".alert:first-child > .alertinner > strong")
+    SUCCESS_MESSAGE = (By.CSS_SELECTOR, ".alert-success:first-child")
